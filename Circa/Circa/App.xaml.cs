f4ba14d0cd27088b8689329d67c7abc7c18295bd
JS Cháfer <201602115@alu.comillas.edu>
2020-03-09 20:08:03 +0100

    Agregar archivos de proyecto.

---
+++ Circa/Circa/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Circa.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Circa
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NewEventPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
